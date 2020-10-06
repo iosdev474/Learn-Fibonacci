1e7705b3b9cd5b3e75d0499e0429879eeb739468
Lakshya Tyagi <tyagicoolboy22@gmail.com>
2020-10-06 23:38:20 +0530

    Indentation Correction

--- fib_Javascript.js
+++ fib_Javascript.js
@@ -1,19 +1,16 @@
 <script type = "text/javascript"> 
-function fibonacci(num) 
-{ 
-	var num1=0; 
-	var num2=1; 
-	var sum; 
-	var i=0; 
-	for (i = 0; i < num; i++) 
+	function fibonacci(num) 
 	{ 
-		sum=num1+num2; 
-		num1=num2; 
-		num2=sum; 
+		var num1 = 0, num2 = 1, sum, i = 0; 
+		for (i = 0; i < num; i++) 
+		{ 
+			sum = num1 + num2; 
+			num1 = num2; 
+			num2 = sum; 
+		} 
+		return num2; 
 	} 
-	return num2; 
-} 
 
-document.write("Fibonacci(5): "+fibonacci(5)+"<br>"); 
-document.write("Fibonacci(8): "+fibonacci(8)+"<br>"); 
+	document.write("Fibonacci(5): " + fibonacci(5) + "<br>"); 
+	document.write("Fibonacci(8): " + fibonacci(8) + "<br>"); 
 </script> 
